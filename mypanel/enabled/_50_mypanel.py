3412eef92cb10b6d7a68f4054b76414835dbaa90
Jim Lin <jimlintw922@gmail.com>
2016-09-21 10:18:15 +0000

    fixed a bug

--- mypanel/enabled/_50_mypanel.py
+++ mypanel/enabled/_50_mypanel.py
@@ -7,4 +7,7 @@ DISABLED = False
 # A list of applications to be added to INSTALLED_APPS.
 ADD_INSTALLED_APPS = [
     'mydashboard',
+    'mydashboard.mypanel',
+    'mydashboard.mypanel2',
+    'mydashboard.mypanel3',
 ]
